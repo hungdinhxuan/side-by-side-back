7faae451bf6cb9d9f740cff6f4f2617442d89ccd
GM <dinhhungGM>
2021-04-28 22:12:44 +0700

    Started

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) =>{
+    res.json({message: 'Hello world'})
+})
+
+app.listen(PORT)
