5f513fbef12d25fa62cd2a5d17915a0a470b6b04
GM <dinhhungGM>
2021-04-30 22:04:22 +0700

    fix vercel

--- index.js
+++ index.js
@@ -8,7 +8,40 @@ const atlasDB = "mongodb+srv://admin:admin@cluster0.rrcyu.mongodb.net/myFirstDat
 const localDB = "mongodb://localhost:27017/SideBySideDB"
 
 
-authentication(app)
+// authentication(app)
+const passport = require("passport");
+  const LocalStrategy = require("passport-local").Strategy;
+  const renters = require("./models/renters");
+  const bcrypt = require("bcryptjs");
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      renters.findOne({ username: username }, function (err, renter) {
+        if (err) {
+          return done(err);
+        }
+        if (!renter) {
+          return done(null, false);
+        }
+        if (!bcrypt.compareSync(password, renter.password)) {
+          return done(null, false);
+        }
+        return done(null, renter);
+      });
+    })
+  );
 
 app.use(express.urlencoded({ extended: true }))
 
