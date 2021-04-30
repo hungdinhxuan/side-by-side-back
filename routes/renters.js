434bdf2b2198a73018c86cdc21a31ee8072828fb
GM <dinhhungGM>
2021-04-30 22:22:44 +0700

    test

--- routes/renters.js
+++ routes/renters.js
@@ -5,7 +5,7 @@ const renterController = require('../controllers/renters')
 
 
 router.get('/', renterController.get)
-// router.post('/login', require('../controllers/login'))
-router.post('/register', renterController.post)
+router.post('/login', require('../controllers/login'))
+// router.post('/register', renterController.post)
 
 module.exports = router
