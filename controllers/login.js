d2d20feaa19803b2d7b7b62327f96ed9deb8f54c
GM <dinhhungGM>
2021-04-30 22:38:23 +0700

    test on vercel

--- controllers/login.js
+++ controllers/login.js
@@ -4,20 +4,19 @@ const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const fs = require("fs")
 const path = require('path')
-const privateKey = fs.readFileSync(path.join(__dirname,  '../key.pem'))
-const publicKey = fs.readFileSync(path.join(__dirname,  '../public.pem'))
+const privateKey = fs.readFileSync(path.join(__dirname,  '../keys/key.pem'))
+const publicKey = fs.readFileSync(path.join(__dirname,  '../keys/public.pem'))
 
 module.exports = (req, res, next) => {
   passport.authenticate("local", (err, renter) => {
     if (err) res.json("Loi server");
     if (!renter) res.json("Username or password is not correct");
     else {
-      // const token = jwt.sign({ id: renter._id }, privateKey, {
-      //   algorithm: "RS256",
-      // });
-      // res.json({ token: token });
+      const token = jwt.sign({ id: renter._id }, privateKey, {
+        algorithm: "RS256",
+      });
+      res.json({ token: token });
       
-      res.json(renter)
     }
   })(req, res, next);
 };
