2c491623e2139368e96b691747c8a122519a5543
GM <dinhhungGM>
2021-05-06 22:39:06 +0700

    -m

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const fs = require("fs")
 const privateKey = fs.readFileSync(path.join(__dirname,  '/keys/key.pem'))
 const publicKey = fs.readFileSync(path.join(__dirname,  '/keys/public.pem'))
-const frontendHost = 'https://side-by-side-front.vercel.app'
+const frontendHost = 'http://localhost:3000'
 const atlasDB =
   "mongodb+srv://admin:admin@cluster0.rrcyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const localDB = "mongodb://localhost:27017/SideBySideDB";
