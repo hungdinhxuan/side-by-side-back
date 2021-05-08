329c3fa037b908d9a64a971f775471d3d2b3eb31
GM <dinhhungGM>
2021-05-08 16:27:44 +0700

    -m

--- routes/renters.js
+++ routes/renters.js
@@ -4,7 +4,7 @@ const renterController = require('../controllers/renters')
 const path = require('path')
 const verify = require('../verify')
 
-router.get('/', verify, renterController.get)
+router.get('/', renterController.get)
 router.post('/login', require('../controllers/login'))
 router.post('/register', renterController.post)
 
