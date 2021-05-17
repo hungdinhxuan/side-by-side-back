9fb836e8b21b4ef493affe23a32b989a052ded98
GM <dinhhungGM>
2021-05-17 16:21:21 +0700

    try remove

--- routes/players.js
+++ routes/players.js
@@ -2,13 +2,10 @@ const express = require("express");
 const router = express.Router();
 const PlayerController = require("../controllers/players");
 const verify = require("../middleware/verify");
-
 const Player = require("../models/Player");
 const Renter = require("../models/Renter");
-
 const axios = require("axios");
-const lineByLine = require('n-readlines')
-const liner = new lineByLine('./imglink.txt')
+
 
 
 // @route GET /api/player/create
@@ -57,30 +54,30 @@ router.get("/create",  (req, res) => {
     });
 });
 
-router.get('/create2', async (req, res) => {
-  let line
-  try {
-    const renter = await Renter.find({})  
-    for (let i = 0; i < renter.length ; i++) {
-        line = liner.next()
-        const player = await Player.create({
-          avatar:line.toString('utf-8'),
-                  firstName: "",
-                  lastName: "",
-                  sex: "unknown",
-                  city: "Ho Chi Minh",
-                  nation: "Viet Nam",
-                  price: 50000 + Math.floor(Math.random() * 100000),
-                  renterId: renter[i]._id,
-        })
-        console.log(player)
-    }
-    return res.json({success: true, message: 'Created Player'});
-  } catch (error) {
-    return res.status(500).json({ success: false, message: 'Internal Server Error'});
-  }
+// router.get('/create2', async (req, res) => {
+//   let line
+//   try {
+//     const renter = await Renter.find({})  
+//     for (let i = 0; i < renter.length ; i++) {
+//         line = liner.next()
+//         const player = await Player.create({
+//           avatar:line.toString('utf-8'),
+//                   firstName: "",
+//                   lastName: "",
+//                   sex: "unknown",
+//                   city: "Ho Chi Minh",
+//                   nation: "Viet Nam",
+//                   price: 50000 + Math.floor(Math.random() * 100000),
+//                   renterId: renter[i]._id,
+//         })
+//         console.log(player)
+//     }
+//     return res.json({success: true, message: 'Created Player'});
+//   } catch (error) {
+//     return res.status(500).json({ success: false, message: 'Internal Server Error'});
+//   }
   
-})
+// })
 
 // @route GET /api/player/destroy
 // @desc destroy all Player
--- test.js
+++
@@ -1,7 +0,0 @@
-const lineByLine = require('n-readlines')
-const liner = new lineByLine('./imglink.txt')
-
-let line
-while(line = liner.next()) {
-    console.log(line.toString('utf-8'))
-}
