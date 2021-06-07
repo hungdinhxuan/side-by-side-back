aa75af0f73e4799c116d322e824468126600e371
GM <dinhhungGM>
2021-06-07 18:36:36 +0700

    FIX Store files

--- controllers/uploadImages.js
+++ controllers/uploadImages.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'public/images'))
+    cb(null, path.join(__dirname, '../public/images'))
   },
   filename: (req, file, cb) => {
     cb(
