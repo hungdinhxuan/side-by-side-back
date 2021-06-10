d5f5ccaba023bd0b47fe1e323475ea8cf6bfd596
dinhhungGM <dinhhungGM@gmail.com>
2021-06-10 14:16:35 +0700

    fix socket

--- socket.js
+++ socket.js
@@ -3,36 +3,133 @@ module.exports = (io) => {
   const listPlayersOnline = []
   const Player = require('./models/Player')
   const Renter = require('./models/Renter')
+  const { publicKey } = require('./config')
+  const jwt = require('jsonwebtoken')
 
   io.on('connection', (socket) => {
     /* … */
-    console.log(socket.id + 'connected')
+    // console.log(socket.id + 'connected')
     socket.on('disconnect', () => {
-      console.log(socket.id + 'disconnected')
+      // console.log(socket.id + 'disconnected')
     })
-    socket.on('validation', async (token) => {
+    socket.on('xac-thuc', async (token) => {
       try {
         const decoded = await jwt.verify(token, publicKey)
+
         const { renterId } = decoded
-        listRentersOnline.push({ renterId: socket.id })
+        let obj = {}
+        obj[renterId] = socket.id
+        listRentersOnline.push(obj)
+
         const player = await Player.findOne({ renterId })
+
         if (player) {
-          const playerId = player._id
-          const socketId = socket.id
-          listPlayersOnline.push({ playerId: socketId })
+          const listPlayersOnlineId = listPlayersOnline.map(
+            (value) => Object.keys(value)[0]
+          )
+          /// Nếu mà player chưa có trong danh sách online
+
+          if (!(player._id in listPlayersOnlineId)) {
+            obj = {}
+
+            obj[player._id] = socket.id
+
+            listPlayersOnline.push(obj)
+          } /// Nếu mà player có trong danh sách online nhưng mà kết nối ở socket khác thì cập nhật lại socket
+          else {
+            listPlayersOnline.forEach((value, index) => {
+              Object.keys(value).map((key) => {
+                if (key === player.id && socket.id != value[key]) {
+                  listPlayersOnline[index][key] = socket.id
+                }
+              })
+            })
+          }
+          let playersOnl = []
+          let playersOffline = []
+          try {
+            const listPlayersOnlineId = listPlayersOnline
+              .map((value) => Object.keys(value).map((key) => key))
+              .map((value) => value[0])
+
+            playersOnl = await Player.find({
+              _id: { $in: listPlayersOnlineId },
+            }).limit(50).lean()
+          } catch (error) {
+            playersOnl = []
+          }
+          try {
+            const listPlayersOnlineId = listPlayersOnline
+              .map((value) => Object.keys(value).map((key) => key))
+              .map((value) => value[0])
+            const playersOffline = await Player.find({
+              _id: { $nin: listPlayersOnlineId },
+            }).limit(50 - listPlayersOnlineId.length).lean()
+            playersOnl.forEach(
+              (value, index) => playersOnl[index].status = 'online'
+            )
+            
+            playersOffline.forEach(
+              (value, index) => playersOffline[index].status = 'offline'
+            )
+            const listPlayers = playersOnl.concat(playersOffline)
+            
+            io.sockets.emit('showPlayers', {
+              success: true,
+              response: listPlayers,
+            })
+          } catch (error) {
+            playersOffline = []
+          }
         }
       } catch (error) {
         socket.emit('error', {
           success: false,
-          message: 'Token is not valid',
-          error: err,
+          response: 'Token is not valid',
+          error: error,
         })
       }
     })
-    
-    socket.on('getListPlayers', () => {
-        
-    })
 
+    socket.on('getListPlayers', async () => {
+      let playersOnl = []
+      let playersOffline = []
+      try {
+        const listPlayersOnlineId = listPlayersOnline
+          .map((value) => Object.keys(value).map((key) => key))
+          .map((value) => value[0])
+
+        playersOnl = await Player.find({
+          _id: { $in: listPlayersOnlineId },
+        }).limit(50).lean()
+      } catch (error) {
+        playersOnl = []
+      }
+      try {
+        const listPlayersOnlineId = listPlayersOnline
+          .map((value) => Object.keys(value).map((key) => key))
+          .map((value) => value[0])
+        const playersOffline = await Player.find({
+          _id: { $nin: listPlayersOnlineId },
+        }).limit(50 - listPlayersOnlineId.length).lean()
+
+        playersOnl.forEach(
+          (value, index) => playersOnl[index].status = 'online'
+        )
+
+        playersOffline.forEach(
+          (value, index) => playersOffline[index].status = 'offline'
+        )
+        const listPlayers = playersOnl.concat(playersOffline)
+        
+
+        io.sockets.emit('showPlayers', {
+          success: true,
+          response: listPlayers,
+        })
+      } catch (error) {
+        playersOffline = []
+      }
+    })
   })
 }
--- test.js
+++ test.js
@@ -6,5 +6,11 @@ function getRandomInt(min, max) {
 
   // "src": "/(.*)",
             // "dest": "/"
-  
+const {publicKey} = require('./config')
+const jwt = require('jsonwebtoken')
+
+const test = async () =>{
+  console.log(await jwt.verify('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZW50ZXJJZCI6IjYwYmRkYjNjMTA5ZThjMjc3NDk2NmRjZiIsImlhdCI6MTYyMzI0OTE0MX0.zc9g7hZEFlkGnsE8USeuDGw_Y0eSDapuxb59XMoPobXY8pSJ3rZopOjhGds8Mpho332CF9Ik3S7-jQA2zabems5r1DgtEBCe3wPWY02vS61nTSwhFsaJo6WgLKLC52EQTdIfZpvGbniqieXNE8nWq3w5O4TDXtGSLFjUiow41tERBFJFTb5OPFLPZzz7WfNCWxTZPBknlqMuwN3KcTtrNvOFCfycTSnlNO4Z9PtzuuiEG5_ydY522u2I8aoApJmAvYAfIVO3zehTCoNhkUwv3aAwe5TtO5AyaPie9FdMAX5_n8PAPpn0CyuxWQC20WNBdZl0PYDauisq890yoN5jEA', publicKey))
+}
+test()
   
